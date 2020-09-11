9987f190596afafc7bc71d2f0acd5786edd09091
Kit Transue <src@kittransue.net>
2020-09-11 07:52:56 -0700

    Connect endpoint to port; monitor events w/ callback.

--- Sources/XTouchCA/main.swift
+++ Sources/XTouchCA/main.swift
@@ -19,10 +19,38 @@ do {
         print("Sources present: \(n)")
         assert(n == 1)  // with X-Touch present
     }
-
+    
     do {
         let n = MIDIGetNumberOfDestinations()
         print("Destinations present: \(n)")
         assert(n == 1)  // with X-Touch present
     }
+}
+
+do {
+    var client = MIDIClientRef()
+    let clientResult = MIDIClientCreate("MIDI subsystem client" as CFString, nil, nil, &client)
+    guard clientResult == noErr else {
+        fatalError("MIDIClientCreate error: \(clientResult)")
+    }
+    
+    var inputPort = MIDIPortRef()
+    let portResult = MIDIInputPortCreate(client, "input port" as CFString, midiEventCallback, nil, &inputPort)
+    guard portResult == noErr else {
+        fatalError("MIDIInputPortCreate error: \(portResult)")
+    }
+    
+    let sourceEndpoint = MIDIGetSource(0)
+    let bindResult = MIDIPortConnectSource(inputPort, sourceEndpoint, nil)
+    guard bindResult == noErr else {
+        fatalError("MIDIPortConnectSource error: \(bindResult)")
+    }
+    
+    print("Entering run loop")
+    RunLoop.current.run(until: Date(timeIntervalSinceNow: 10))
+    print("Finished run loop")
+}
+
+func midiEventCallback(_ packets: UnsafePointer<MIDIPacketList>, _ readProcRefCon: UnsafeMutableRawPointer?, _ srcConnRefCon: UnsafeMutableRawPointer?) {
+    print("callback!")
 }
