e6bf88e8a147c5727174d613ccf00d4cd2c1dab7
Kit Transue <src@kittransue.net>
2023-01-05 17:35:15 -0800

    Refactor common button handling into base class.

--- Sources/ControlSurface/IndicatorButton.swift
+++ Sources/ControlSurface/IndicatorButton.swift
@@ -33,30 +33,15 @@ public class SurfaceButton: SurfaceControl, MomentaryButton {
     }
 }
 
-public class IndicatorButton: SurfaceControl, MomentaryButton, SurfaceIndicator {
-    weak var endpoint: MidiEndpoint?
-    let midiAddress: UInt8
-    
+public class IndicatorButton: SurfaceButton, SurfaceIndicator {
     @Published
     public var indicator: IndicatorState = .off {
         didSet {
             endpoint?.sendMidi(message: MidiMessage(subject: .buttonMC, id: self.midiAddress, value: indicator.rawValue))
         }
     }
-    
-    @Published
-    public private(set) var isPressed = false
-    
-    init(endpoint: MidiEndpoint, address: UInt8) {
-        self.endpoint = endpoint
-        self.midiAddress = address
-    }
 
-    public func action(message: MidiMessage) {
-        guard message.subject == .buttonMC else {
-            logger.warning("button got unexpected action \(message)")
-            return
-        }
-        isPressed = message.value != 0
+    override init(endpoint: MidiEndpoint, address: UInt8) {
+        super.init(endpoint: endpoint, address: address)
     }
 }
