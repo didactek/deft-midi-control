0298faf87fae9132f7e6b1e79707ca5d1c4ce6c1
Kit Transue <src@kittransue.net>
2023-01-06 22:04:16 -0800

    Make unified encoder a MidiResponder.

--- Sources/ControlSurface/SurfaceRotaryEncoder.swift
+++ Sources/ControlSurface/SurfaceRotaryEncoder.swift
@@ -9,14 +9,19 @@ import Foundation
 
 /// An object that represents all the functions of the control knob: a rotary delta encoder,
 /// a pushbutton swtich, and a ring indicator.
-class SurfaceRotaryEncoder {
+class SurfaceRotaryEncoder: MidiResponder {
     let delta: DeltaEncoder
-    let button: MomentaryButton
+    let button: SurfaceButton
     let indicator: CircularIndicator
     
-    init(delta: DeltaEncoder, button: MomentaryButton, indicator: CircularIndicator) {
+    init(delta: DeltaEncoder, button: SurfaceButton, indicator: CircularIndicator) {
         self.delta = delta
         self.button = button
         self.indicator = indicator
+    }
+    
+    func action(message: MidiMessage) {
+        delta.action(message: message)
+        button.action(message: message)
     }
 }
