e927b9140ef5245f64e246a1e49cad3a7309a24c
Kit Transue <src@kittransue.net>
2023-01-07 09:49:41 -0800

    Conditionalize main experiments in MIDI API.

--- Sources/XTouchCA/main.swift
+++ Sources/XTouchCA/main.swift
@@ -5,30 +5,32 @@
 //  Created by Kit Transue on 2020-09-10.
 //
 
-import CoreMIDI
+import Foundation
 import ControlSurface
 import Combine
 
+#if false // true: show a little bit more of how MIDI works
+import CoreMIDI
 do {
     do {
         let n = MIDIGetNumberOfExternalDevices()
         print("Devices present: \(n)")
         assert(n == 0)  // even with X-Touch present, not shown as an external device
     }
-    
+
     do {
         let n = MIDIGetNumberOfSources()
         print("Sources present: \(n)")
         assert(n == 1)  // with X-Touch present
     }
-    
+
     do {
         let n = MIDIGetNumberOfDestinations()
         print("Destinations present: \(n)")
         assert(n == 1)  // with X-Touch present
     }
 }
-
+#endif
 
 do {
     let surface = try! XTouchMiniMC(midiSourceIndex: 0)
